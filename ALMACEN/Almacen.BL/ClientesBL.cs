28af1df4c2ca7f54c1bee684fd58a46e51efc394
yosep <yosep@DESKTOP-MKDPRPD>
2021-04-12 15:28:29 -0600

    Agregando plantilla a nuestro sitio WEB

---
+++ ALMACEN/Almacen.BL/ClientesBL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Quinto Avance
+namespace Almacen.BL
+{
+    public class ClientesBL
+    {
+
+        Contexto _contexto;
+        public List<Clientes> ListadeClientes { get; set; }
+
+        public ClientesBL()
+        {
+            _contexto = new Contexto();
+            ListadeClientes = new List<Clientes>();
+        }
+
+        public List<Clientes> ObtenerClientes()
+        {
+            ListadeClientes = _contexto.Clientes.OrderBy(r=>r.Nombre).ToList();
+
+            return ListadeClientes;
+        }
+        public List<Clientes> ObtenerClientesActivo()
+        {
+            ListadeClientes = _contexto.Clientes.Where(s=> s.Activo==true).OrderBy(r => r.Nombre).ToList();
+
+            return ListadeClientes;
+        }
+
+
+
+        public void GuardarClientes(Clientes clientes)
+        {
+
+            if (clientes.Id == 0)
+            {
+                _contexto.Clientes.Add(clientes);
+            }
+            else
+            {
+                var clientesExistente = _contexto.Clientes.Find(clientes.Id);
+                clientesExistente.Nombre = clientes.Nombre;
+                clientesExistente.Telefono = clientes.Telefono;
+                clientesExistente.Direccion = clientes.Direccion;
+                clientesExistente.Activo = clientes.Activo;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Clientes ObtenerClientes(int id)
+        {
+            var clientes = _contexto.Clientes.FirstOrDefault(p => p.Id == id);
+
+            return clientes;
+        }
+
+        public void EliminarClientes(int Id)
+        {
+            var clientes = _contexto.Clientes.Find(Id);
+
+            _contexto.Clientes.Remove(clientes);
+
+            _contexto.SaveChanges();
+
+        }
+
+    }
+}
---
+++ ALMACEN/Almacen.BL/ServiciosBL.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.BL
+{
+    public class ServiciosBL
+    {
+        Contexto _contexto;
+        public List<Servicio> ListadeServicios { get; set; }
+
+        public ServiciosBL()
+        {
+            _contexto = new Contexto();
+            ListadeServicios = new List<Servicio>();
+        }
+
+        public List<Servicio> ObtenerServicios()
+        {
+            ListadeServicios = _contexto.Servicios.Include("Categoria").OrderBy(r=>r.Categoria.Descripcion).ThenBy(r=> r.Descripcion).ToList();
+
+            return ListadeServicios;
+        }
+        public List<Servicio> ObtenerServiciosActivos()
+        {
+            ListadeServicios = _contexto.Servicios.Include("Categoria").Where(s => s.Activo == true).OrderBy(r=>r.Descripcion).ToList();
+
+            return ListadeServicios;
+        }
+
+
+
+        public void GuardarServicio(Servicio servicio)
+        {
+
+            if (servicio.Id == 0)
+            {
+                _contexto.Servicios.Add(servicio);
+            }
+            else
+            {
+                var productoExistente = _contexto.Servicios.Find(servicio.Id);
+
+                productoExistente.Descripcion = servicio.Descripcion;
+                productoExistente.CategoriaId = servicio.CategoriaId;
+                productoExistente.Precio = servicio.Precio;
+                productoExistente.UrlImagen = servicio.UrlImagen;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Servicio ObtenerServicio(int id)
+        {
+            var servicio = _contexto.Servicios.Include("Categoria").FirstOrDefault(p => p.Id == id);
+
+            return servicio;
+        }
+
+        public void EliminarServicio(int Id)
+        {
+            var servicio = _contexto.Servicios.Find(Id);
+
+            _contexto.Servicios.Remove(servicio);
+
+            _contexto.SaveChanges();
+            
+        }
+
+    }
+}
